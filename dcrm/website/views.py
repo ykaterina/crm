fb5f106b72eba526d18d8c2fc81e2c35362a0719
catherinebarrientos <barrientoscb05@gmail.com>
2023-11-03 22:43:29 +0800

    view individual record

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -49,3 +49,12 @@ def register_user(request):
         return render(request, 'register.html', {'form': form})
         
     return render(request, 'register.html', {'form': form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #lookup record
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, 'You must logged be in to view this page')
+        return redirect('home')
